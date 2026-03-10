1783cb13092593565504e5024d83e66e6e4f5065
jalforja-gif <jalfojra@gmail.com>
2026-03-10 23:49:02 +0800

    Update ml_api_app.py

--- ml_api_app.py
+++ ml_api_app.py
@@ -117,6 +117,7 @@ def ping():
 # ==============================
 # RUN SERVER
 # ==============================
-# if __name__ == '__main__':
-
-#    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
