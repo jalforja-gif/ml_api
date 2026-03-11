c1505fc742619500f47fd9b6b4091f1f0b7d4549
jalforja-gif <jalfojra@gmail.com>
2026-03-11 10:01:03 +0800

    Change default port from 5000 to 10000

--- serve.py
+++ serve.py
@@ -2,7 +2,8 @@ from waitress import serve
 from ml_api_app import app
 import os
 
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 10000))
 
 print(f"Starting ML API on 0.0.0.0:{port} ...")
 serve(app, host="0.0.0.0", port=port)
+
