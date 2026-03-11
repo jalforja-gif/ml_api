b1549510f37b76f7b4ac3b42defc8a106a4e0aa1
jalforja-gif <jalfojra@gmail.com>
2026-03-11 10:21:20 +0800

    Refactor and enhance ML API application

--- ml_api_app.py
+++ ml_api_app.py
@@ -91,3 +91,9 @@ def predict():
 @app.route("/ping")
 def ping():
     return "ML API is running!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
